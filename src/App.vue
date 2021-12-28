6b54b9eb6f16af78605b340d5a854caa53913afe
viniciusbatalha <89221817+viniciusbatalha@users.noreply.github.com>
2021-12-28 10:00:50 -0300

    importando outro projeto em vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <barra-lateral/>
+    </div>
+    <div class="column is-three-quarter">
+      <formulario/>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/Formulario.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { 
+    BarraLateral,
+    Formulario
+    },
+});
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="box">
+        <div class="clomuns">
+            <div class="column is-8" role="form" aria-label="Formulário para uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa deseja inserir?"/>
+            </div>
+            <div class="column">
+                <temporizador/>
+            </div>
+        </div>
+    </div>
+</template> 
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Temporizador from './Temporizador.vue'
+
+export default defineComponent({
+    name: 'Formulário',
+    components: {
+        Temporizador
+    }
+})
+</script>
+
+<style>
+
+</style>
